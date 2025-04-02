4d17bdef69a68f5cddda8ccd050bb34bcea337a5
Adrian Zinko <zinko.adrian00@gmail.com>
2025-04-02 22:41:12 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { PUBLIC_CONVEX_URL } from '$env/static/public';
+	import { setupConvex } from 'convex-svelte';
+	import '../app.css';
+
+	const { children } = $props();
+	setupConvex(PUBLIC_CONVEX_URL);
+</script>
+
+{@render children()}
